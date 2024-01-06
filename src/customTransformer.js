b705e777b5871d14b63b91131c20d37e75981d64
劉尚謙 <55793374+kennysuper007@users.noreply.github.com>
2024-01-07 03:07:06 +0800

    Update customTransformer.js

--- src/customTransformer.js
+++ src/customTransformer.js
@@ -319,11 +319,8 @@ async function embed(block) {
                     console.error("PDF file block with unsupported google url: ", url);
                     return false;
                 }
-                const url = getUrlFromFileOrExternalBlock(pdf, 'pdf');
-                if (!url) return false;
-                iframe = `<iframe src="${url}" style="width: 100%; margin:0; aspect-ratio: 16/9;"></iframe>`;
-                const caption_div = caption ? CAPTION_DIV_TEMPLATE.replace("{{caption}}", caption) : "";
-                iframe =  `<div class="embed">${iframe}${caption_div}</div>`;
+                iframe = `<iframe src="${url}" style="width: 100%; margin:0; aspect-ratio: 16/9;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+                break;
             default:
                 console.warn("Embed block with unsupported domain, url: ", url);
                 iframe = `<iframe src="${url}" style="width: 100%; margin:0; aspect-ratio: 16/9;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
